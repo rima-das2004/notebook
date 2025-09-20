96301fcfb25766726c8347e5702212c4e63579be
rima-das2004 <rima983131@gmail.com>
2025-09-20 23:30:03 +0530

    first commit

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
+const API_BASE = import.meta.env.VITE_API_BASE || "https://notes-manager-backend-yrkj.onrender.com/api";
 
 const api = axios.create({ baseURL: API_BASE });
 
